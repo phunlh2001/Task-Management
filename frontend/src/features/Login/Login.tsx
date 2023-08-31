25fda99948279c120b6b3d492f4ca6e74945d5fd
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2023-08-31 09:00:24 +0700

    feat: building base for auth

--- frontend/src/features/Login/Login.tsx
+++ frontend/src/features/Login/Login.tsx
@@ -1,16 +1,26 @@
-import React, { useState } from 'react'
-import { Routes, Route, Link } from 'react-router-dom';
-import "./Login.css"
+import React, { useState } from 'react';
+import "./Login.css";
 import LoginBox from './LoginBox/components/LoginBox';
 import Register from './Register/components/Register';
+import { useActions } from 'hooks/useActions';
+import { authAction } from './authSlice';
+import { Account } from 'models';
 
 type Props = {
   setToken:(val:boolean)=> void
-  };
+};
 
 const Login: React.FC<Props> = ({setToken}) => {
   
+  const { login } = useActions(authAction)
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
   const[showRegister, setShowRegister] = useState(false)
+
+  const handleLogin = async (value: Account) => {
+    login(value)
+  }
   
   return (
     <div className='login_page'>
